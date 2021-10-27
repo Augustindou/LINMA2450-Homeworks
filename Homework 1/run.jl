f449f76fa174a0c757ed3793bdb99a950a486b02
Augustindou <augustin.doultremontao@gmail.com>
2021-10-27 16:13:25 +0200

    run

--- Homework 1/run.jl
+++ Homework 1/run.jl
@@ -1,6 +1,7 @@
 using JSON
 
 include("gurobi_solution.jl")
+include("greedy_algorithm.jl")
 
 DATA_PATH = "small.json"
 data = JSON.parsefile(DATA_PATH)
@@ -15,3 +16,11 @@ println("----------------------")
 println("------- Gurobi -------")
 println("----------------------")
 println(gurobi_knapsack(utilities, weights, b))
+
+println("\n")
+
+# run greedy
+println("----------------------")
+println("------- Greedy -------")
+println("----------------------")
+println(greedy_knapsack(utilities, weights, b))
